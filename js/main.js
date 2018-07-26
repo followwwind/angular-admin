9f683f32a222ae2935c0ecb930ac32b8cbb0cbdf
followwwind <18771933975@163.com>
2018-07-26 23:35:18 +0800

    初始化系统参数

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ angular.module('app')
 
       // config
       $scope.app = {
-        name: 'Angulr',
+        name: 'Angular',
         version: '2.0.1',
         // for chart colors
         color: {
